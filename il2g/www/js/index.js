cae622534a5f5380acbcef6b76899067e81712c1
damorenon <damorenon@gmail.com>
2014-05-04 19:17:21 -0500

    Remake del proyecto, se agregan herramientas pro

--- il2g/www/js/index.js
+++ il2g/www/js/index.js
@@ -1,31 +1,49 @@
-document.addEventListener("deviceready", onDeviceReady, false);
-
-function onDeviceReady() {
-    console.log("aquí internacionalizar")
-}
-
-
-
 /*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
 
-var url1 = "http://jesussalvamifamilia.org/il2g/getOneQuote.json.php?idioma=1";
-var url2 = "http://localhost:3000/rest/il2g/getOneQuote.json.php?idioma=1";
-var url3 = "http://localhost:3000/rest/il2g/getOneQuote.json.php";
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
 
-$(document).on("click","#consultar", function(){
-	console.log("hiciste click");
-	$.ajax({
-	  type: "GET",
-	  url: url3,
-	  datatype: "json"
-	}).done(function(data){
-		alert("la rta: " + JSON.stringify(data) )
-	}).error(function(jqXHR, textStatus, m) {
-    	alert("jqXHR: " + JSON.stringify(jqXHR) +" textStatus: "+ textStatus);
-    });
-});
-
-*/
-
-
-	
+        console.log('Received Event: ' + id);
+    }
+};
--- server/server.js
+++ server/server.js
@@ -1,41 +1,11 @@
-var baseDir = '../il2g/www'
-var http = require('http');
-var fs = require('fs');
-
-if(process.argv[2] == 'proxy'){	
-	console.log('running server as proxy');
-	var proxy = require('./proxyService');
-}else{
-	console.log('running server with mocks');
-	var proxy = require('./mockService');
-}
+var http = require('http'),
+    st   = require('node-static'),
+    opts = { cache: false },
+    file = new st.Server('../il2g/www', opts),
+    port = process.env.PORT || 3000;
 
 http.createServer(function (req, res) {
-	var url = req.url;
-	if(url.match(/^\/rest\/.*/)){
-		proxy.invoke(req, res);
-	}else{
-		try{
-			if(url == '/'){
-				url = '/index.html';
-			}
-			if(url=='terminate'){
-				procces.exit();
-			}
-			var response = fs.readFileSync(baseDir + url);
-			var fileType = url.replace(/.*\.(.+)/, "$1");
-			var contentType = 'text/plain';
-			switch(fileType){
-				case 'html': contentType = 'text/html'; break;
-				case 'css' : contentType = 'text/css'; break;
-				case 'js' : contentType = 'text/javascript'; break;
-				case 'otf' : contentType = 'application/octet-stream'; break;
-			}
-			res.writeHead(200, "OK", {'Content-Type': contentType});
-			res.end(response);
-		}catch(e){
-			res.writeHead(404);
-			res.end();
-		}
-	}
-}).listen(3000);
+    file.serve(req, res);
+}).listen(port);
+
+console.log("App running on http://localhost:%s", port);
